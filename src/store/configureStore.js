0f036087adc14ed5ed19acbbcce6200d76ceb63e
Andy Ho <89andy@gmail.com>
2019-02-03 20:03:01 +0800

    Add redux thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import usersReducer from "./reducers/users";
 
@@ -7,7 +8,7 @@ const rootReducer = combineReducers({
 });
 
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, applyMiddleware(thunk));
 };
 
 export default configureStore;
