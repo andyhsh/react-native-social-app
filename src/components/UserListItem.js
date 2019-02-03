725a6ee3ac18139e026132d3680e85cf9d4a8866
Andy Ho <89andy@gmail.com>
2019-02-03 21:56:13 +0800

    Add UserListItem component

---
+++ src/components/UserListItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { colors } from "../styles/theme";
+
+class UserListItem extends Component {
+  render() {
+    const { id, name, onPress } = this.props;
+
+    return (
+      <TouchableOpacity style={styles.container} onPress={() => onPress(id)}>
+        <View>
+          <Text>{name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 40,
+    paddingVertical: 30,
+    backgroundColor: colors.white,
+  }
+});
+
+UserListItem.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  onPress: PropTypes.func
+};
+
+export default UserListItem;
