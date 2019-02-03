b2230dbaa7685c04f9c2baf7c67153da7a892a0a
Andy Ho <89andy@gmail.com>
2019-02-03 17:31:06 +0800

    Set up basic stacks and tabs navigation

--- App.js
+++ App.js
@@ -1,50 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from "react";
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+  createAppContainer
+} from "react-navigation";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { UserListScreen } from "./src/screens/UserListScreen";
+import { UserProfileScreen } from "./src/screens/UserProfileScreen";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const headerStyling = {
+  headerStyle: {
+    backgroundColor: "#4ccace"
+  },
+  headerTintColor: "#ffffff",
+  headerTitleStyle: {
+    fontWeight: "bold"
+  }
+};
 
-type Props = {};
-export default class App extends Component<Props> {
+const UserListStack = createStackNavigator(
+  {
+    UserList: {
+      screen: UserListScreen
+    },
+    UserProfile: {
+      screen: UserProfileScreen
+    }
+  },
+  {
+    initialRouteName: "UserList",
+    defaultNavigationOptions: headerStyling
+  }
+);
+
+const UserProfileStack = createStackNavigator(
+  {
+    UserProfile: {
+      screen: UserProfileScreen
+    }
+  },
+  {
+    initialRouteName: "UserProfile",
+    defaultNavigationOptions: headerStyling
+  }
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Friends: { screen: UserListStack },
+    Profile: { screen: UserProfileStack }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#000",
+      inactiveTintColor: "#454f5b"
+    }
+  }
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
